af9abacc42ebcfaebd542fc180360b439f997594
Thando Somacele <thando@searchkingsafrica.com>
2024-06-12 16:50:21 +0200

    plugnin works with sessionStorage implemented

--- ska-utm-grabber.js
+++ ska-utm-grabber.js
@@ -1,27 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-  function processUrl() {
-      // Check if the URL contains UTM parameters
-      if (!window.location.href.includes('utm')) return;
-
-      const originalUrl = window.location.href;
-      const originalUrlObject = new URL(originalUrl);
-      const queryString = originalUrlObject.search;
-
-      // Get the anchor tag element by its ID
-      const sudonimATag = document.getElementById('sudonim-link');
-      const newBaseUrl = utmGrabberData.baseUrl;
-
-      // Append UTM parameters to the new base URL
-      let newUrl;
-      if (newBaseUrl.includes('?')) {
-          const modifiedQueryString = queryString.replace('?', '&');
-          newUrl = newBaseUrl + modifiedQueryString;
-          sudonimATag.href = newUrl;
-      } else {
-          newUrl = newBaseUrl + queryString;
-          sudonimATag.href = newUrl;
-      }
-  }
-
-  processUrl();
-});
+    function processUrl() {
+        const originalUrl = window.location.href;
+        const originalUrlObject = new URL(originalUrl);
+        const queryString = originalUrlObject.search;
+  
+        // Check if the URL contains UTM parameters
+        if (queryString.includes('utm')) {
+            // Store the UTM parameters in sessionStorage
+            sessionStorage.setItem('utmParams', queryString);
+        }
+  
+        // Retrieve the UTM parameters from sessionStorage
+        const storedQueryString = sessionStorage.getItem('utmParams');
+  
+        if (storedQueryString) {
+            // Get the anchor tag element by its ID
+            const sudonimATag = document.getElementById('sudonim-link');
+            const newBaseUrl = utmGrabberData.baseUrl;
+  
+            // Append UTM parameters to the new base URL
+            let newUrl;
+            if (newBaseUrl.includes('?')) {
+                const modifiedQueryString = storedQueryString.replace('?', '&');
+                newUrl = newBaseUrl + modifiedQueryString;
+                sudonimATag.href = newUrl;
+            } else {
+                newUrl = newBaseUrl + storedQueryString;
+                sudonimATag.href = newUrl;
+            }
+        }
+    }
+  
+    processUrl();
+  });
