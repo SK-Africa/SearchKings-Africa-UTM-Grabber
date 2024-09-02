519ec8785a829e7a3ea47d31c15ea8c78a79df9d
Thando Somacele <thando@searchkingsafrica.com>
2024-09-02 16:31:29 +0200

    remove sudonim features cleanup and updated gitignore

--- includes/class-ska-utm-grabber-admin.php
+++ includes/class-ska-utm-grabber-admin.php
@@ -15,11 +15,8 @@ class SKA_UTM_Grabber_Admin {
         include_once SKA_UTM_GRABBER_PATH . 'admin/partials/ska-utm-grabber-admin-display.php';
     }
 
-    public function register_and_build_fields() {
-        // Register settings
-        register_setting( 'utmGrabber', 'utm_grabber_base_url' );
-        register_setting( 'utmGrabber', 'utm_grabber_show_icon' );
-        register_setting( 'utmGrabber', 'utm_grabber_link_class' );
+    public function register_and_build_fields()
+    {
 
         // Add settings section
         add_settings_section(
@@ -28,52 +25,10 @@ class SKA_UTM_Grabber_Admin {
             array( $this, 'utm_grabber_settings_section_callback' ),
             'utmGrabber'
         );
-
-        // Add settings fields
-        add_settings_field(
-            'utm_grabber_base_url',
-            __( 'Base URL', 'ska-utm-grabber' ),
-            array( $this, 'utm_grabber_base_url_render' ),
-            'utmGrabber',
-            'utm_grabber_section'
-        );
-
-        add_settings_field(
-            'utm_grabber_show_icon',
-            __( 'Show WhatsApp Icon', 'ska-utm-grabber' ),
-            array( $this, 'utm_grabber_show_icon_render' ),
-            'utmGrabber',
-            'utm_grabber_section'
-        );
-
-        add_settings_field(
-            'utm_grabber_link_class',
-            __( 'Link Class', 'ska-utm-grabber' ),
-            array( $this, 'utm_grabber_link_class_render' ),
-            'utmGrabber',
-            'utm_grabber_section'
-        );
     }
 
     public function utm_grabber_settings_section_callback() {
         echo __( 'Enter the settings for the SearchKings Africa UTM Grabber plugin.', 'ska-utm-grabber' );
-    }
-
-    public function utm_grabber_base_url_render() {
-        $base_url = get_option( 'utm_grabber_base_url', 'https://rply.link/d/27600899357/SKWA?Link=https://searchkingsafrica.com/' );
-        ?>
-        <input type='text' name='utm_grabber_base_url' value='<?php echo esc_attr( $base_url ); ?>' style="width: 80%; margin-bottom: 20px;" placeholder='<?php echo esc_attr( $base_url ); ?>'>
-        <?php
-    }
-
-    public function utm_grabber_show_icon_render() {
-        $show_icon = get_option( 'utm_grabber_show_icon', 'yes' );
-        ?>
-        <select name='utm_grabber_show_icon'>
-            <option value='yes' <?php selected( $show_icon, 'yes' ); ?>><?php _e( 'Yes', 'ska-utm-grabber' ); ?></option>
-            <option value='no' <?php selected( $show_icon, 'no' ); ?>><?php _e( 'No', 'ska-utm-grabber' ); ?></option>
-        </select>
-        <?php
     }
 
 }
--- uninstall.php
+++ uninstall.php
@@ -4,9 +4,4 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
 
-// Delete plugin options
-delete_option( 'utm_grabber_base_url' );
-delete_option( 'utm_grabber_show_icon' );
-delete_option( 'utm_grabber_link_class' );
-
 // Perform any other cleanup necessary for your plugin
